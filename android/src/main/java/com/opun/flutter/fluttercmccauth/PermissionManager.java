858a57364c9d87668ef2d59735ef2a001122f216
woguava <woguava@sina.com>
2019-03-20 12:57:36 +0800

    androidX

--- android/src/main/java/com/opun/flutter/fluttercmccauth/PermissionManager.java
+++ android/src/main/java/com/opun/flutter/fluttercmccauth/PermissionManager.java
@@ -4,8 +4,8 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
 import android.widget.Toast;
+import androidx.core.app.ActivityCompat;
 
 import io.flutter.plugin.common.PluginRegistry;
 
